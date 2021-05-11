f04d9cc61ce4eff8e38ef35d2653d998c1db205f
SN-Vidya <snvidya1998@gmail.com>
2021-05-11 10:28:59 +0530

    second commit

---
+++ springrememberme/src/main/java/com/spring/rememberme/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.spring.rememberme;
+import org.springframework.security.web.context.*;          
+public class SecurityWebApplicationInitializer    
+    extends AbstractSecurityWebApplicationInitializer {    
+}  
+
